38b7e2a5295ab69aa533be6617ab20d13fdbed5b
ycanerol <ycanerol@gmail.com>
2023-01-31 16:05:55 +0300

    Change to case-insensitive extension

--- extract_exif.sh
+++ extract_exif.sh
@@ -5,7 +5,7 @@ rm $1/temperatures.txt
 for folder in $folders
 do
 	echo "$folder" >> $1/temperatures.txt
-	exiftool -p '$FileName $DateTimeOriginal $CameraTemperature' $1/$folder/*.CR2 | sed -r 's/:/-/' | sed -r 's/:/-/'>> $1/temperatures.txt
+	exiftool -p '$FileName $DateTimeOriginal $CameraTemperature' -ext cr2 $1/$folder/ | sed -r 's/:/-/' | sed -r 's/:/-/'>> $1/temperatures.txt
 done
 
 
